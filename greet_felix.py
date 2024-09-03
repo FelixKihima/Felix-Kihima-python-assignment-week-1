ff33bac7e3b2d19534be49746e5594a20e127b0c
FelixKihima <kihimafelix75@gmail.com>
2024-09-03 13:17:47 -0700

    Answers to week 1 assignment

---
+++ greet_felix.py
@@ -0,0 +1,8 @@
+
+name = ("Felix Kihima")
+age = 21
+location = ("California")
+
+message = f"Hello {name} you are {age} years old and live in {location}, Keep mastering Python King!"
+
+print(message)
